e0ec15e6c5cebc25abd25b2798fcbd416ed285a0
jbkun069 <codyr4111@gmail.com>
2025-09-03 23:22:25 +0530

    Initial visualization setup

---
+++ visualization.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
+import numpy as np
+import calendar
+from datetime import datetime
+
+# Set style for matplotlib and seaborn
+plt.style.use('default')
+sns.set_palette("viridis")
